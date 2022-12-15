be2ada36aa4a2cc2ffef9ecd526aa5e3b201cf96
Michał Sujkowski <michalsujkowski.fd@gmail.com>
2022-12-15 23:24:51 +0100

    add window outerWidth

--- untitled/article-preview-component-master/app.js
+++ untitled/article-preview-component-master/app.js
@@ -1,11 +1,22 @@
 const shareBtn = document.querySelector('.arrow');
 const shareIcons = document.querySelector('.share-icons');
+const personInfo = document.querySelector('.person-info');
 
 const shareFunc = () => {
     shareIcons.classList.toggle('active');
+    if (window.outerWidth <= 775) {
+        personInfo.style.opacity = '0';
+        shareIcons.style.zIndex = "100";
+    }
 }
 
 shareBtn.addEventListener('click', shareFunc);
+
 shareIcons.addEventListener('click', () => {
     shareIcons.classList.remove('active');
+    if (window.outerWidth <= 775) {
+        personInfo.style.opacity = '1';
+        shareIcons.style.zIndex = "-100";
+    }
 });
+
